a491d84b18d78768d332164415338d7d47a5f9b0
AdAntonio07 <antonio.ferreira1320@gmail.com>
2024-01-03 16:22:05 -0300

    Criado componete de icone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,17 @@
 import styles from './Header.module.css'
 
-import Title from "../title/Title"
-import Subtitle from "../subtitle/Subtitle"
+import Title from '../title/Title'
+import Subtitle from '../subtitle/Subtitle'
+import Icon from '../icon/Icon'
 
 function Header() {
   return (
     <div className={styles.header}>
       <Title>Jogo da Velha</Title>
       <Subtitle>Criado por Antonio Ferreira</Subtitle>
+      <div className={styles.iconContent}>
+        <Icon iconName="github" link="https://github.com/dashboard" />
+      </div>
     </div>
   )
 }
