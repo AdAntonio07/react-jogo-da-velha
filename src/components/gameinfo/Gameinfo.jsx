fc6ddfbde91dac7252b9e867033e8e6c09a1db4a
AdAntonio07 <antonio.ferreira1320@gmail.com>
2024-01-08 16:16:54 -0300

    Criado logica para declara um campião e reset

---
+++ src/components/gameinfo/Gameinfo.jsx
@@ -0,0 +1,48 @@
+import styles from './Gameinfor.module.css'
+
+import Icon from '../icon/Icon'
+import Button from '../button/Button'
+
+function Gameinfo({ currentPlayer, winner, onReset }) {
+
+  const shouldEnableButton = () => {
+    if (winner !== 0) return true
+  }
+
+  return (
+    <div className={styles.gameProximo}>
+      {
+        winner === 0 &&
+        <>
+          <h4>Próximo a jogar:</h4>
+          {
+            currentPlayer === 1 && <Icon iconName="circle" />
+          }
+          {
+            currentPlayer === -1 && <Icon iconName="x" />
+          }
+        </>
+      }
+      {
+        winner !== 0 &&
+        <>
+          <h4>Fim de Jogo! Campeão:</h4>
+          {
+            winner === 1 && <Icon iconName="circle" />
+          }
+          {
+            winner === -1 && <Icon iconName="x" />
+          }
+        </>
+      }
+      <Button
+        onClick={onReset}
+        disabled={!shouldEnableButton()}
+      >
+        Reiniciar
+      </Button>
+    </div>
+  )
+}
+
+export default Gameinfo
