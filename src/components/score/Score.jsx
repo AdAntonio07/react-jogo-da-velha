20b044d500cd479e9dd29259aa89cdb45eeaa482
AdAntonio07 <antonio.ferreira1320@gmail.com>
2024-01-15 17:37:48 -0300

    Criado placar

---
+++ src/components/score/Score.jsx
@@ -0,0 +1,23 @@
+import Style from './Score.module.css'
+
+import Icon from '../icon/Icon'
+
+function Score() {
+  return (
+    <>
+      <h4>Placar:</h4>
+      <div className={Style.score}>
+        <div className={Style.scoreContent}>
+          <Icon iconName="circle" />
+          <h2>0</h2>
+        </div>
+        <div className={Style.scoreContent}>
+          <Icon iconName="x" />
+          <h2>0</h2>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Score 
